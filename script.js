2bd522d2daf6c35070061339d5d4c78db80fb1ce
Ujjwal Gupta <ujjgpta9@gmail.com>
2021-07-03 12:52:36 +0530

    fixed a mistake

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ toggleStatus=false;
             final.results[i].contents + "</div><hr>" + `<a class='apply-link' href=${final.results[i].refs.landing_page}><button class='apply-btn'>ApplyHere</button></a></div>`;
             }
             document.getElementById('here').innerHTML += "<br><br><hr><br><div id='prev-next-container'><button class='prev-next-btn' onclick='prev()'>prev</button><button class='prev-next-btn' onclick='next()'>next</button></div>";
-            document.getElementById('#back-to-top').style.display='block';
+            document.getElementById('back-to-top').style.display='block';
         }
         catch (e) {
             console.log(e);
